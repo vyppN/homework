9592ae606639ec33958e02edcfd02a101a1c29ef
vyppN <chwrtokuma@gmail.com>
2023-07-05 02:01:25 +0700

    fix: change the return type of factory to interface

--- src/mocks/factory.ts
+++ src/mocks/factory.ts
@@ -1,12 +1,11 @@
 import {CalculatorFactory} from "../utils/factory";
 import {User} from "../internal/domain/user";
-
-import {FoodCalculator} from "../service/calculator";
+import {FoodCalculatorService} from "../internal/port/service";
 
 export class CalculatorFactorySpy extends CalculatorFactory {
     public setIsMember: boolean
 
-    override createWithMemberShip(user: User): FoodCalculator {
+    override createWithMemberShip(user: User): FoodCalculatorService {
         this.setIsMember = user.hasMemberCard
         return super.createWithMemberShip(user);
     }
--- src/utils/factory.ts
+++ src/utils/factory.ts
@@ -1,5 +1,6 @@
 import {User} from '../internal/domain/user'
 import {Discountable} from '../internal/port/pricing'
+import {FoodCalculatorService} from '../internal/port/service'
 import {MembershipDiscount} from '../repository/discount'
 import {FoodFormMockDataSource} from '../repository/food'
 import {FoodCalculator} from '../service/calculator'
@@ -11,7 +12,7 @@ export class CalculatorFactory {
         this.defaultDiscountOptions = defaultDiscountOptions || []
     }
 
-    createWithMemberShip(user: User): FoodCalculator {
+    createWithMemberShip(user: User): FoodCalculatorService {
         if(user.hasMemberCard) this.defaultDiscountOptions.push(new MembershipDiscount(10))
         const repo = new FoodFormMockDataSource(this.defaultDiscountOptions)
 
